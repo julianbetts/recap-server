92ebd8d68e994560b54a8ddf105e3bf87068d6c4
Julian Betts <wjulianbetts@gmail.com>
2022-12-04 19:55:41 -0800

    added dogs json

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express')
+const express = require('express');
+const data = require('./db/dogs.json');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/api/dogs', (req, res) => {
+    // console.log(req);
+    res.json(data)
+})
 
 app.listen(3001, () => console.log("app is runnin on http://localhost:3001"))
